4f7c92746e6f51d2a3f907f503152468be52c0f0
nico <nico@nicos-imac-pro.fritz.box>
2020-05-18 01:37:13 +0200

    Adventure Assignment addet

--- Assignment3/Advanture.java
+++ Assignment3/Advanture.java
@@ -1,5 +1,65 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
 import acm.program.ConsoleProgram;
 
-public class Advanture extends ConsoleProgram{
+public class Advanture extends ConsoleProgram {
+	private HashMap<String, ArrayList<String>> roomMap = new HashMap<String, ArrayList<String>>();
+	private BufferedReader br;
 
+	public void run() {
+		readRoomMapFile();
+		startAdventure();
+
+	}
+
+	private void startAdventure() {
+		String currentRoom = "hallway";
+		while (true) {
+			println("You are currently in " + currentRoom);
+			println("You can go to " + roomMap.get(currentRoom));
+			String target = readLine("Go to: ");
+			if(roomMap.containsKey(target)) {
+				currentRoom = target;
+			}
+			else
+				println("please enter vaild room");
+
+		}
+	}
+
+	private void readRoomMapFile() {
+		try {
+			br = new BufferedReader(new FileReader("Lab4/roomMap.txt"));
+			while (true) {
+				String line = br.readLine();
+				if (line == null) {
+					break;
+				}
+				fillMapWithData(line);
+			}
+			br.close();
+		} catch (IOException e) {
+			println("file not found");
+		}
+
+	}
+
+	private void fillMapWithData(String line) {
+		ArrayList<String> rooms;
+		StringTokenizer st = new StringTokenizer(line, ">");
+		String from = st.nextToken().trim();
+		String to = st.nextToken().trim();
+		if (roomMap.containsKey(from)) {
+			rooms = roomMap.get(from);
+		} else {
+			rooms = new ArrayList<String>();
+		}
+		rooms.add(to);
+		roomMap.put(from, rooms);
+	}
 }
