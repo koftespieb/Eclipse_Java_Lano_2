285947b9e93ef9aec8782f4636fc73964f0d134b
nico <nico@imac-pro.fritz.box>
2020-05-20 21:48:02 +0200

    lightning bug fix

--- Assignment4/Lightning.java
+++ Assignment4/Lightning.java
@@ -12,7 +12,7 @@ public class Lightning extends GraphicsProgram {
 	private static final int Y_P2 = SIZE / 2 - 70;
 
 	boolean start = false;
-	public double displacement = 100;
+	public double displacement = 240;
 
 	public void init() {
 		setSize(SIZE, SIZE);
@@ -22,18 +22,16 @@ public class Lightning extends GraphicsProgram {
 	public void run() {
 		drawPoint(X_P1, Y_P1);
 		drawPoint(X_P2, Y_P2);
-		drawLightning(X_P1 + 15, Y_P1 + 15, X_P2 + 15, Y_P2 + 15, displacement);
+		while (true) {
+			drawLightning(X_P1 + 15, Y_P1 + 15, X_P2 + 15, Y_P2 + 15, displacement);
+			pause(140);
+		}
 
 	}
 
 	private void drawLightning(double x1, double y1, double x2, double y2, double displacement) {
-		if (start) {
-			drawLine(x1, y1, x2, y2);
-		}
-		start = true;
-
 		// base case
-		if (displacement < 0.2) {
+		if (displacement < 2) {
 			drawLine(x1, y1, x2, y2);
 		} else {
 			double mid_x = (x1 + x2) / 2.0;
@@ -44,18 +42,19 @@ public class Lightning extends GraphicsProgram {
 			drawLightning(x2, y2, mid_x, mid_y, displacement / 2);
 
 		}
-		pause(10);
 	}
 
 	private void drawLine(double x1, double y1, double x2, double y2) {
 		GLine line = new GLine(x1, y1, x2, y2);
-		line.setColor(Color.white);
+		line.setColor(Color.cyan);
 		add(line);
+		line.sendToBack();
 
 	}
 
 	private void drawPoint(int x, int y) {
 		GOval p = new GOval(30, 30);
+		p.setColor(Color.white);
 		p.setFilled(true);
 		p.setFillColor(Color.white);
 		add(p, x, y);
