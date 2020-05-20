78969163e9b8c39a095434357838d1d52cb6d4db
nico <nico@imac-pro.fritz.box>
2020-05-20 21:32:37 +0200

    lightning added

---
+++ Assignment4/Lightning.java
@@ -0,0 +1,64 @@
+import java.awt.Color;
+
+import acm.graphics.GLine;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+public class Lightning extends GraphicsProgram {
+	private static final int SIZE = 500;
+	private static final int X_P1 = 60;
+	private static final int X_P2 = 440;
+	private static final int Y_P1 = SIZE / 2;
+	private static final int Y_P2 = SIZE / 2 - 70;
+
+	boolean start = false;
+	public double displacement = 100;
+
+	public void init() {
+		setSize(SIZE, SIZE);
+		setBackground(Color.DARK_GRAY);
+	}
+
+	public void run() {
+		drawPoint(X_P1, Y_P1);
+		drawPoint(X_P2, Y_P2);
+		drawLightning(X_P1 + 15, Y_P1 + 15, X_P2 + 15, Y_P2 + 15, displacement);
+
+	}
+
+	private void drawLightning(double x1, double y1, double x2, double y2, double displacement) {
+		if (start) {
+			drawLine(x1, y1, x2, y2);
+		}
+		start = true;
+
+		// base case
+		if (displacement < 0.2) {
+			drawLine(x1, y1, x2, y2);
+		} else {
+			double mid_x = (x1 + x2) / 2.0;
+			double mid_y = (y1 + y2) / 2.0;
+			mid_x += (Math.random() - 0.5) * displacement;
+			mid_y += (Math.random() - 0.5) * displacement;
+			drawLightning(x1, y1, mid_x, mid_y, displacement / 2);
+			drawLightning(x2, y2, mid_x, mid_y, displacement / 2);
+
+		}
+		pause(10);
+	}
+
+	private void drawLine(double x1, double y1, double x2, double y2) {
+		GLine line = new GLine(x1, y1, x2, y2);
+		line.setColor(Color.white);
+		add(line);
+
+	}
+
+	private void drawPoint(int x, int y) {
+		GOval p = new GOval(30, 30);
+		p.setFilled(true);
+		p.setFillColor(Color.white);
+		add(p, x, y);
+
+	}
+}
