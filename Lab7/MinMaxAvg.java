ed010419b5e492a0f0b31dc4dfaf0bf04d3f9bef
nico <nico@imac-pro.fritz.box>
2020-05-25 02:52:27 +0200

    MinMaxAvg Improvement

--- Lab7/MinMaxAvg.java
+++ Lab7/MinMaxAvg.java
@@ -1,26 +1,64 @@
+import java.lang.reflect.Array;
+
 import acm.program.ConsoleProgram;
+import acm.util.RandomGenerator;
 
 public class MinMaxAvg extends ConsoleProgram {
 	private static final int SIZE = 500;
-	private int[] arrayOfInts = { 1, 5, 22, 6, 9, 99, 7, -3, 6 };
+	private static final int ARRAY_SIZE = 10000000;
+	private int[] arrayOfInts = new int[ARRAY_SIZE];
+	public RandomGenerator rgen = new RandomGenerator();
 
 	public void init() {
 		setSize(SIZE, SIZE);
 	}
 
 	public void run() {
-		println(getMin(arrayOfInts));
+		for (int i = 0; i < arrayOfInts.length; i++) {
+			arrayOfInts[i] = rgen.nextInt(-1000000, 1000000);
+		}
+
+		getMin(arrayOfInts);
+		getMax(arrayOfInts);
+		getAVG(arrayOfInts);
 
 	}
 
-	private int getMin(int[] arrayOfInts) {
-		int m = arrayOfInts[0];
+	private void getAVG(int[] arrayOfInts) {
+		final long start = System.currentTimeMillis();
+		double ges = 0;
+		for (int i = 0; i < arrayOfInts.length; i++) {
+			ges += arrayOfInts[i];
+		}
+		double avg = ges / arrayOfInts.length;
+		final long end = System.currentTimeMillis();
+
+		println("AVG: " + avg + "		Finisehd in: " + (end - start) + " Ms\n");
+	}
+
+	private void getMax(int[] arrayOfInts) {
+		final long start = System.currentTimeMillis();
+		int max = arrayOfInts[0];
 		for (int i = 1; i < arrayOfInts.length; i++) {
-			if (m > arrayOfInts[i]) {
-				m = arrayOfInts[i];
+			if (max < arrayOfInts[i]) {
+				max = arrayOfInts[i];
 			}
 		}
-		return m;
+		final long end = System.currentTimeMillis();
+		println("MAX: " + max + "		Finisehd in: " + (end - start) + " Ms\n");
+	}
+
+	private void getMin(int[] arrayOfInts) {
+		final long start = System.currentTimeMillis();
+		int min = arrayOfInts[0];
+		for (int i = 1; i < arrayOfInts.length; i++) {
+			if (min > arrayOfInts[i]) {
+				min = arrayOfInts[i];
+			}
+		}
+		final long end = System.currentTimeMillis();
+		println("MIN: " + min + "		Finisehd in: " + (end - start) + " Ms\n");
 
 	}
+
 }
