022c708d2714ca455237535bef9802dcce424e8a
nico <nico@imac-pro.fritz.box>
2020-06-08 17:12:31 +0200

    tree pi/2 added as angle

--- Assignment4/Tree.java
+++ Assignment4/Tree.java
@@ -9,6 +9,7 @@ public class Tree extends GraphicsProgram {
 	private static final int SIZE = 500;
 	private static final double X_START = SIZE / 2;
 	private static final double Y_START = SIZE - 24;
+	private static final double START_ANGLE = Math.PI / 2.0;
 	private static final double STAMM_LAENGE = 100;
 
 	public void init() {
@@ -16,14 +17,14 @@ public class Tree extends GraphicsProgram {
 	}
 
 	public void run() {
-		drawAst(X_START, Y_START, 1.5708, STAMM_LAENGE);
+		drawAst(X_START, Y_START, START_ANGLE, STAMM_LAENGE);
 	}
 
 	private void drawAst(double x, double y, double angle, double length) {
 		double x1 = x - Math.cos(angle) * length;
 		double y1 = y - Math.sin(angle) * length;
 
-		drawLine(x, y, x1, y1,length);
+		drawLine(x, y, x1, y1, length);
 
 		// base case
 		if (length < 10) {
@@ -36,12 +37,11 @@ public class Tree extends GraphicsProgram {
 		drawAst(x1, y1, angle + bendAngle + branchAngle, length * rgen.nextDouble(0.6, 0.8));
 	}
 
-	private void drawLine(double x, double y, double x1, double y1,double length) {
+	private void drawLine(double x, double y, double x1, double y1, double length) {
 		GLine line = new GLine(x, y, x1, y1);
-		if(length < 15) {
+		if (length < 15) {
 			line.setColor(Color.GREEN);
-		}
-		else {
+		} else {
 			line.setColor(Color.black);
 		}
 		add(line);
