e12c12686785c05a202b179398c639a4dd6aedaa
nico <nico@roam213040.roam.fh-nuernberg.de>
2020-06-30 15:01:11 +0200

    Lab 8 lab9 added

--- Lab8/Searching.java
+++ Lab8/Searching.java
@@ -1,15 +1,27 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
 import acm.program.ConsoleProgram;
 
 public class Searching extends ConsoleProgram {
 	private int[] arr = { 44, 88, 17, 32, 97, 65, 28, 82, 29, 76, 54, 80 };
-	private int[] arrSorted = { 17, 28, 29, 32, 44, 54, 65, 76, 80, 82, 88, 97 };
+	private int[] arrSorted = { 17, 28, 29, 32, 44, 54, 65, 76, 80, 82, 88, 97 ,100,120};
 
 	public void run() {
 		int key = readInt("Enter Int: ");
-		println("An Stelle: " + sequentialSearch(key, arr));
-		println("\nBinary Search: " + binarySearch(key, arrSorted, 0, arrSorted.length));
+		println("\nAn Stelle: " + sequentialSearch(key, arr));
+		println("\nBinary Search an stelle: " + binarySearch(key, arrSorted, 0, arrSorted.length));
+		println("\nHash Search: " + hashSearch(key,arr) );
 	}
-	// geht nicht
+	private boolean hashSearch(int key, int[] arr) {
+		HashSet<Integer> hs = new HashSet<Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			hs.add(arr[i]);
+		}
+		boolean b = hs.contains(key);
+		
+		return b;
+	}
 	private int binarySearch(int key, int[] arrSorted, int start, int stop) {
 		// base case
 		if (start > stop) {
@@ -17,10 +29,11 @@ public class Searching extends ConsoleProgram {
 		}
 		// recursive case
 		int m = (start + stop) / 2;
-		if (arr[m] == key) {
+		
+		if (arrSorted[m] == key) {
 			return m;
 		}
-		else if (key < arr[m]) {
+		else if (key < arrSorted[m]) {
 			return binarySearch(key, arrSorted, start, m - 1);
 		}
 		else{
---
+++ Lab9/Greedy.java
@@ -0,0 +1,26 @@
+import acm.program.ConsoleProgram;
+
+public class Greedy extends ConsoleProgram {
+	int[] coinValue = { 200, 100, 50, 20, 10, 5 };
+	int[] coinAmount = { 3, 2, 1, 2, 15, 4 };
+	int price = 115;
+
+	public void run() {
+		println("Price for one Coffee: " + price);
+		int back = readInt("Enter Ammount in Cent: ") - price;
+		giveChange(back);
+
+	}
+
+	private void giveChange(int back) {
+		for (int i = 0; i < coinValue.length; i++) {
+			// check if coins available
+			while ((coinAmount[i] > 0) && (back - coinValue[i] >= 0)) {
+				back = back - coinValue[i];
+				coinAmount[i]--;
+				print(coinValue[i] + ", ");
+			}
+		}
+
+	}
+}
