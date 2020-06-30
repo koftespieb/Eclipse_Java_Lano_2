163cd466e8647e24240acddf89b9227cb8b5287e
nico <nico@localhost>
2020-06-30 12:07:14 +0200

    rabbits

--- Lab7/Rabbits.java
+++ Lab7/Rabbits.java
@@ -1,8 +1,18 @@
 import acm.program.ConsoleProgram;
 
 public class Rabbits extends ConsoleProgram {
+	long z1 = 0;
+	long z2 = 1;
+	long result = 0;
 
 	public void run() {
-		
+		int end = readInt("Enter int: ");
+		for (int i = 0; i < end; i++) {
+			result = z1 + z2;
+			z2 = z1;
+			z1 = result;
+			println(result);
+
+		}
 	}
 }
