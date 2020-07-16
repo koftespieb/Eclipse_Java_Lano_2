49d75bb440689141a054b069a8bfc11cbcc2abe8
nico <nico@roam213040.roam.fh-nuernberg.de>
2020-07-16 15:35:25 +0200

    crossword added

---
+++ Text/Crossword.java
@@ -0,0 +1,11 @@
+import acm.program.ConsoleProgram;
+
+public class Crossword extends ConsoleProgram{
+	private final String[] words = { "ark", "card", "dad", "day", "dear",
+			"down", "east", "erase", "ever", "father", "itsy", "man", "mesh",
+			"near", "nerf", "send", "stinks", "stun", "sync", "yard" };
+	public void run() {
+		
+	}
+
+}
